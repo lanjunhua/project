64c14fa27c97776c3ec45c9b9802c968cedd8802
lanjunhua <lanjunhua@users.noreply.github.com>
2016-06-25 19:12:10 +0800

    Update ms24StrGenerateFunc.R
    # 11 lines
    previous method is wrong, so write another function to get the result
    
    #18 lines
    cannot use lubridate library for 30~31 and 30~28/29  
    so rewrite the function get the time

--- pbobRecall/R/ms24StrGenerateFunc.R
+++ pbobRecall/R/ms24StrGenerateFunc.R
@@ -8,18 +8,15 @@ msRecallPbocLLD <- function(dfSTPbocLLD, dfRecallPbocPDD, monthOffSet, ...){
     dfSTPbocLLD <- left_join(dfSTPbocLLD, dfRecallPbocPDD, by = c('ID_LTRA' = 'ID_PTRA', 'CATG' = 'CATG'), copy = FALSE)
     dfSTPbocLLD2 <- within(dfSTPbocLLD, {
       if(monthOffSet < 24){
-        remained24MStr <- ifelse(is.na(RCD_RP24M), NA, paste0(unlist(strsplit(RCD_RP24M, ""))[1:(24 - monthOffSet)], collapse = ''))
+       remained24MStr <- msRecallPboc24MRemaindStrFunc(RCDL_RP24M, monthOffSet)
       } else {
         remained24MStr <- NA
       }
       
       TIMEL_LSTINQ <- ifelse(is.na(TIMEL_LSTINQ) & !is.na(TIMEL_SETTLE), TIMEL_SETTLE, TIMEL_LSTINQ)
       
-      TIMEL_SRPRCD24M <- TIMEL_LSTINQ - months(24 + monthOffSet - 1)
-      day(TIMEL_SRPRCD24M) <- 1
-      end24MTime <- TIMEL_LSTINQ - months(monthOffSet)
-      TIMEL_LSTINQ <- end24MTime
-      day(TIMEL_ERPRCD24M) <- 30
+      TIMEL_SRPRCD24M1 <- msSysVarsPbocMonthCal(as.Date(TIMEL_LSTINQ), -(24 + monthOffSet - 1))
+      TIMEL_ERPRCD24M1 <- msSysVarsPbocMonthCal(as.Date(TIMEL_LSTINQ), -monthOffSet)
       
       mapStratTime <- TIMEL_SRPRCD24M 
       mapEndTime <- TIMEL_LSTINQ - months(24)
